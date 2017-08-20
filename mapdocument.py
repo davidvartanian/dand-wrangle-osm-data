383ea12e622e67ab6464a942074fdf2e0a2e3a0a
David Vartanian <davidvartanian@posteo.de>
2017-08-20 13:58:55 +0200

    feat: Add MapDocument class
    - Parse an Element object (from ElementTree)
    - Map values to data structure provided by MongoDB course Problem Set

---
+++ mapdocument.py
@@ -0,0 +1,63 @@
+import re
+import json
+
+
+class MapDocument:
+
+
+    def __init__(self, element=None, jsondoc=None, street_auditor=None):
+        self.street_auditor = street_auditor
+        if element is not None:
+            self.doc = self.parse_element(element)
+        elif jsondoc is not None:
+            self.doc = json.reads(jsondoc)
+
+    def todict(self):
+        return self.doc
+
+    def parse_element(self, element):
+        lower = re.compile(r'^([a-z]|_)*$')
+        lower_colon = re.compile(r'^([a-z]|_)*:([a-z]|_)*$')
+        problemchars = re.compile(r'[=\+/&<>;\'"\?%#$@\,\. \t\r\n]')
+        CREATED = [ "version", "changeset", "timestamp", "user", "uid"]
+        doc = {}
+        if element.tag in ["node", "way"]:
+            doc['type'] = element.tag
+            for a in element.attrib:
+                if a in CREATED:
+                    if 'created' not in doc:
+                        doc['created'] = {}
+                    doc['created'][a] = element.attrib[a]
+                elif a in ('lat','lon'):
+                    if 'pos' not in doc:
+                        doc['pos'] = [0,0]
+                    key = 0 if a == 'lat' else 1
+                    doc['pos'][key] = float(element.attrib[a])
+                else:
+                    doc[a] = element.attrib[a]
+            for t in element.iter('tag'):
+                if problemchars.search(t.attrib['k']):
+                    continue
+                elif t.attrib['k'].startswith('addr:'):
+                    if 'address' not in doc:
+                        doc['address'] = {}
+                    if t.attrib['k'].count(':') == 1:
+                        addr_type = t.attrib['k'].split(':')[1]
+                        if addr_type == 'street' and self.street_auditor is not None:
+                            value = self.street_auditor.update_name(t.attrib['v'])
+                        else:
+                            value = t.attrib['v']
+                        doc['address'][addr_type] = value
+                elif t.attrib['k'].count(':') == 1:
+                    extra_node = t.attrib['k'].split(':')
+                    if extra_node[0] not in doc:
+                        doc[extra_node[0]] = {}
+                    doc[extra_node[0]][extra_node[1]] = t.attrib['v']
+            if element.tag == 'way':
+                for t in element.iter('nd'):
+                    if 'node_refs' not in doc:
+                        doc['node_refs'] = []
+                    if 'ref' in t.attrib:
+                        doc['node_refs'].append(t.attrib['ref'])
+            return doc
+        return None
